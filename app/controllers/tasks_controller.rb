e0032e53386fcd004da980b3cec686d0d2c476fb
maria laura lucanera <mllucanera@gmail.com>
2018-06-12 15:12:46 -0300

    DAY 27 - task manager ok without css

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,19 +1,49 @@
 class TasksController < ApplicationController
 
+before_action :set_task, only: [:show, :edit, :update, :destroy]
+
 # CREATE
+def new
+  @task = Task.new
+end
+
+def create
+  @task = Task.new(task_params)
+  @task.save
+  redirect_to task_path(@task)
+end
 
 # READ
-  def index
-    @tasks = Task.all
-  end
+def index
+  @tasks = Task.all
+end
 
-  def show
-    @task = Task.find(params[:id])
-  end
-
+def show
+end
 
 # UPDATE
+def edit
+end
+
+def update
+  @task.update(task_params)
+  redirect_to task_path(@task)
+end
 
 # DELETE
+def destroy
+  @task.destroy
+  redirect_to tasks_path
+end
+
+private
+
+def task_params
+  params.require(:task).permit(:title, :details, :completed)
+end
+
+def set_task
+  @task = Task.find(params[:id])
+end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,15 +2,17 @@ Rails.application.routes.draw do
 
 
 # CREATE
+  get "tasks/new", to: "tasks#new", as: "new_task"
+  post "tasks", to: "tasks#create"
 
 # READ
   get "tasks", to: "tasks#index"
   get "tasks/:id", to: "tasks#show", as: "task"
 
 # UPDATE
+  get "tasks/:id/edit", to: "tasks#edit", as: "edit_task"
+  patch "tasks/:id", to: "tasks#update"
 
 # DELETE
-
-
-
+  delete "tasks/:id", to: "tasks#destroy"
 end
